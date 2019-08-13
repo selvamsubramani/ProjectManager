b87e7a93c85726c76339d74fa10108546a1050ee
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-13 08:52:53 -0700

    added components for project management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProjectComponent } from './ui/add-project/add-project.component';
+import { AddUserComponent } from './ui/add-user/add-user.component';
+import { AddTaskComponent } from './ui/add-task/add-task.component';
+import { EditTaskComponent } from './ui/edit-task/edit-task.component';
+import { ViewTaskComponent } from './ui/view-task/view-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProjectComponent,
+    AddUserComponent,
+    AddTaskComponent,
+    EditTaskComponent,
+    ViewTaskComponent
   ],
   imports: [
     BrowserModule,
