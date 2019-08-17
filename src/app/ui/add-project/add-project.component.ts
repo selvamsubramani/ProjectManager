f34dc346874668cdcc77edd06ff7afc5a32cf0bd
Subramani <498959@cognizant.com>
2019-08-17 23:35:43 +0530

    project is added

--- src/app/ui/add-project/add-project.component.ts
+++ src/app/ui/add-project/add-project.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/model/project';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-add-project',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
+  project: Project;
+  projects: Project[];
+
   constructor() { }
 
   ngOnInit() {
+    this.project = new Project();
+    this.projects = [
+      {
+        Id: 1, Name: "Project-01", Priority: 5, IsSuspended: false, StartDate: new Date(), EndDate: new Date(),
+        Manager: { Id: 1, FirstName: 'FirstName-01', LastName: 'LastName-01', EmployeeId: 100 }
+      },
+      {
+        Id: 2, Name: "Project-02", Priority: 15, IsSuspended: false, StartDate: new Date(), EndDate: new Date(),
+        Manager: { Id: 2, FirstName: 'FirstName-02', LastName: 'LastName-02', EmployeeId: 200 }
+      }
+    ];
   }
 
 }
--- src/app/ui/add-user/add-user.component.ts
+++ src/app/ui/add-user/add-user.component.ts
@@ -11,6 +11,7 @@ export class AddUserComponent implements OnInit {
 
   user: User;
   users: User[];
+
   constructor() { }
 
   ngOnInit() {
