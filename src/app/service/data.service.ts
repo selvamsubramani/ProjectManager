e5d3ac33e13bcb456684ceeb2d43cdb0bade3803
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-17 20:17:18 -0700

    user module is updated

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router'
+import { environment } from './../../environments/environment'
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  UserAPI: string = environment.serviceUrl + "user/";
+  ProjectAPI: string = environment.serviceUrl + "project/";
+  TaskAPI: string = environment.serviceUrl + "task/";
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.UserAPI + "GetAllUsers");
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(this.UserAPI + "GetUserById/" + id);
+  }
+
+  addUser(user: User) {
+    return this.http.post(this.UserAPI + "CreateUser", user);
+  }
+
+  updateUser(user: User) {
+    return this.http.put(this.UserAPI + "UpdateUser", user);
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete(this.UserAPI + "DeleteUser/" + id);
+  }
+
 }
--- src/app/ui/add-user/add-user.component.ts
+++ src/app/ui/add-user/add-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { User } from 'src/app/model/user';
+import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-add-user',
@@ -12,14 +14,52 @@ export class AddUserComponent implements OnInit {
   user: User;
   users: User[];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.user = new User();
-    this.users = [
-      { Id: 1, FirstName: 'FirstName-01', LastName: 'LastName-01', EmployeeId: 100 },
-      { Id: 2, FirstName: 'FirstName-02', LastName: 'LastName-02', EmployeeId: 200 }
-    ];
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.dataService.getUsers().subscribe(data => this.users = data);
+  }
+
+  createUser() {
+    this.dataService.addUser(this.user).subscribe(
+      res => {
+        console.log(res);
+        this.ngOnInit();
+      },
+      error => console.log(error)
+    );
+  }
+
+  updateUser() {
+    this.dataService.addUser(this.user).subscribe(
+      res => {
+        console.log(res);
+        this.ngOnInit();
+      },
+      error => console.log(error)
+    );
+  }
+
+  editUser(id: number) {
+    this.dataService.getUserById(id).subscribe(
+      data => this.user = data,
+      error => console.log(error)
+    );
+  }
+
+  deleteUser(id: number) {
+    this.dataService.deleteUser(id).subscribe(
+      res => {
+        console.log(res);
+        this.ngOnInit();
+      },
+      error => console.log(error)
+    )
   }
 
 }
