89e85947bf82b683995a2431eba8993bbbd1300b
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-18 11:38:42 -0700

    add and edit tasks are updated

--- src/app/ui/edit-task/edit-task.component.ts
+++ src/app/ui/edit-task/edit-task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/model/task';
 import { User } from 'src/app/model/user';
 import { DataService } from 'src/app/service/data.service';
+import { Project } from 'src/app/model/project';
 
 @Component({
   selector: 'app-edit-task',
@@ -20,6 +21,12 @@ export class EditTaskComponent implements OnInit {
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.task = new Task();
+    this.task.Owner = new User();
+    this.task.Project = new Project();
+    this.task.Parent = new Task();
+    this.task.Priority = 0;
+
     this.dataService.currentTaskId.subscribe(
       data => this.taskId = data,
       error => console.log(error)
@@ -27,9 +34,17 @@ export class EditTaskComponent implements OnInit {
     
     if (this.taskId > 0) {
       this.dataService.getTaskById(this.taskId).subscribe(
-        data => this.task = data,
+        data => {
+          this.task = data;
+          if (!this.task.Project)
+            this.task.Project = new Project();
+          if (!this.task.Owner)
+            this.task.Owner = new User();
+          if (!this.task.Parent)
+            this.task.Parent = new Task();
+        },
         error => console.log(error));
-      }
+    }
 
     this.getUsers();
     this.getParentTasks();
