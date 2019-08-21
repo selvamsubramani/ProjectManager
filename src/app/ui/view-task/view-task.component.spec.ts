4d0a01bac30639727b94fba26f2498d96123ed29
Subramani <498959@cognizant.com>
2019-08-21 19:16:46 +0530

    View task is done

--- src/app/ui/view-task/view-task.component.spec.ts
+++ src/app/ui/view-task/view-task.component.spec.ts
@@ -1,16 +1,71 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { Observable, of } from 'rxjs';
+import { jqxListBoxComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxlistbox';
 
 import { ViewTaskComponent } from './view-task.component';
+import { Project } from 'src/app/model/project';
+import { User } from 'src/app/model/user';
+import { Task } from 'src/app/model/task';
+import { DataService } from 'src/app/service/data.service';
 
 describe('ViewTaskComponent', () => {
+  class MockDataService {
+    Projects: Project[] = [
+      {
+        Id: 1, Name: 'Project-01', Priority: 5, StartDate: null, EndDate: null, IsDateEnabled: false, IsSuspended: false, NoOfTasks: 5, Status: 'In progress',
+        Manager: { Id: 1, FirstName: 'FirstName-01', LastName: 'LastName-01', EmployeeId: 100 }
+      },
+      {
+        Id: 2, Name: 'Project-02', Priority: 10, StartDate: null, EndDate: null, IsDateEnabled: false, IsSuspended: false, NoOfTasks: 5, Status: 'In progress',
+        Manager: { Id: 2, FirstName: 'FirstName-02', LastName: 'LastName-02', EmployeeId: 200 }
+      }
+    ];
+
+    Tasks: Task[] = [
+      {
+        Id: 1, Name: 'Task-01', Priority: 1, StartDate: new Date(), EndDate: new Date(), IsCompleted: false, IsParent: false,
+        Owner: new User(),
+        Parent: new Task(),
+        Project: {
+          Id: 1, Name: 'Project-01', Priority: 5, StartDate: null, EndDate: null, IsDateEnabled: false, IsSuspended: false, NoOfTasks: 5, Status: 'In progress',
+          Manager: { Id: 1, FirstName: 'FirstName-01', LastName: 'LastName-01', EmployeeId: 100 }
+        }
+      }
+    ];
+
+    getProjects(): Observable<Project[]> {
+      return of(this.Projects);
+    }
+
+    getTasksByProjectId(id: number): Observable<Task[]> {
+      return of(this.Tasks);
+    }
+
+    getProjectById(id: number): Observable<Project> {
+      const project = this.Projects.find(p => p.Id == 1);
+      return of(project);
+    }
+  }
+
   let component: ViewTaskComponent;
   let fixture: ComponentFixture<ViewTaskComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ViewTaskComponent ]
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule],
+      declarations: [ViewTaskComponent,
+        jqxListBoxComponent],
+      providers: [
+        { provide: DataService, useClass: MockDataService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/ui/view-task/view-task.component.ts
+++ src/app/ui/view-task/view-task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { DataService } from 'src/app/service/data.service';
+import { Project } from 'src/app/model/project';
 
 @Component({
   selector: 'app-view-task',
@@ -7,9 +10,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewTaskComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[];
+  projects: Project[];
+  project: Project;
+  projectId: number;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.getProjects();
+  }
+
+  getTasks() {
+    this.dataService.getTasksByProjectId(this.projectId).
+      subscribe(
+        data => this.tasks = data,
+        error => console.log(error));
+  }
+
+  getProjects() {
+    this.dataService.getProjects().
+      subscribe(
+        data => this.projects = data,
+        error => console.log(error));
+  }
+
+  selectProject(event: any): void {
+    if (event.args) {
+      let item = event.args.item;
+      if (item) {
+        this.projectId = item.value;
+      }
+    }
+  }
+
+  setProject() {
+    this.dataService.getProjectById(this.projectId).subscribe(
+      data => {
+        this.project = data;
+        this.getTasks();
+      }, error => console.log(error));
+  }
+
+  endTask(id: number) {
+    this.dataService.completeTask(id).
+      subscribe(
+        res => {
+          console.log(res);
+          this.ngOnInit();
+        },
+        error => console.log(error));
+  }
+
+  deleteTask(id: number) {
+    this.dataService.deleteTask(id).
+      subscribe(
+        res => {
+          console.log(res);
+          this.ngOnInit();
+        },
+        error => console.log(error));
+  }
+
+  editTask(id: number) {
+    this.dataService.changeTask(id);
+  }
+
+  sortByStartDate() {
+    this.tasks.sort((s, t) => {
+      if (!s.StartDate && !t.StartDate)
+        return 0;
+      if (s.StartDate && !t.StartDate)
+        return 1;
+      if (!s.StartDate && t.StartDate)
+        return -1;
+      if (s.StartDate > t.StartDate)
+        return 1;
+      if (s.StartDate < t.StartDate)
+        return -1;
+      return 0;
+    });
+  }
+
+  sortByEndDate() {
+    this.tasks.sort((s, t) => {
+      if (!s.EndDate && !t.EndDate)
+        return 0;
+      if (s.EndDate && !t.EndDate)
+        return 1;
+      if (!s.EndDate && t.EndDate)
+        return -1;
+      if (s.EndDate > t.EndDate)
+        return 1;
+      if (s.EndDate < t.EndDate)
+        return -1;
+      return 0;
+    });
+  }
+
+  sortByPriority() {
+    this.tasks.sort((s, t) => s.Priority - t.Priority);
+  }
+
+  sortByStatus() {
+    this.tasks.sort((s, t) => s.IsCompleted === t.IsCompleted ? 0 : s.IsCompleted? -1 : 1);
   }
 
 }
